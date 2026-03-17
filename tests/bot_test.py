38a1bddee8bcc86ec219a74ed1223bf226a31684
Anna Liberty <liberty.817@proton.me>
2026-03-17 15:58:29 -0700

    Bring bot test coverage to 100%
    Run black to format

--- tests/bot_test.py
+++ tests/bot_test.py
@@ -2,6 +2,8 @@
 Tests the bot model.
 """
 
+import pytest
+
 from models.deck import Wild, Skip, Number, Color
 from models import bot
 
@@ -17,3 +19,16 @@ def test_play_choose():
 
     assert index == 0
     assert color is not None
+
+
+def test_invalid_bot_strategy():
+    """
+    Test trying to use an invalid bot strategy
+    """
+    top = Number(Color.YELLOW, 10)
+    hand = [Wild(None), Number(Color.BLUE, 5), Number(Color.GREEN, 0), Skip(Color.RED)]
+
+    with pytest.raises(bot.BotError) as e:
+        bot.play_card(100, hand, top)
+
+    assert str(e.value) == "Invalid bot strategy chosen"
